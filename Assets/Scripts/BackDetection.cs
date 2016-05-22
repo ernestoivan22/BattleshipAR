c95666e734907c0ebe1549089a7f82ceab60b24d
ernestoivan22 <ernestoivan22@hotmail.com>
2016-05-22 12:03:21 -0600

    Menu Principal

---
+++ Assets/Scripts/BackDetection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+
+                //quit application on return button
+
+                //Application.Quit();
+                Application.LoadLevel(0);
+                return;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+    public bool empezar = false;
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        
+    }
+    void OnMouseUp()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+        if (empezar)
+        {
+            
+            Application.LoadLevel(1);
+
+        }
+        else
+        {
+
+        }
+
+    }
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+
+                //quit application on return button
+
+                Application.Quit();
+                
+                return;
+
+            }
+        }
+    }
+}

e190cb41d73f940e182b981bb8aacc5ae8a010e1
ernestoivan22 <ernestoivan22@hotmail.com>
2016-05-22 22:57:34 -0600

    grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+    public GameObject plane;
+    public int width = 10;
+    public int height = 10;
+    private GameObject[,] grid;
+
+    void Awake()
+    {
+        grid = new GameObject[width, height];
+        for (int x =0; x < width; x++)
+        {
+            for(int z = 0; z < height; z++)
+            {
+                GameObject gridPlane = (GameObject)Instantiate(plane);
+                gridPlane.transform.position = new Vector3(gridPlane.transform.position.x + gridPlane.transform.localScale.x*10*x,
+                    gridPlane.transform.position.y, gridPlane.transform.position.z - plane.transform.localScale.z*10*z);
+                grid[x, z] = gridPlane;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,10,150,100),"Delete grid [3,3]"))
+        {
+            Destroy(grid[3, 3]);
+        }
+    }
+}
